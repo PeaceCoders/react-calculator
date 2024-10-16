abf47c42063e73bf677aff3de63bd6f21cc1f806
sachin <sachink26427>
2024-10-16 22:39:55 +0530

    Initial React project commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Calculator from "./Calculator";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>React Calculator</h1>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
